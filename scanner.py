3bdc1b4a6a32ff51bff24d26eb748e4dd30eef0e
Ellen Hebert <ellenhebert@icloud.com>
2017-10-15 19:43:48 -0400

    Prettify scanner.py

--- scanner.py
+++ scanner.py
@@ -1,3 +1,9 @@
+'''
+    Just a CLI program designed to scan for bluetooth devices surrounding
+    the local machine, then pretty print the services running on them
+    out onto the terminal.
+'''
+
 from bluetooth import *
 from pprint import *
 
@@ -5,9 +11,11 @@ print 'Scanning for devices...'
 
 try:
     for device in discover_devices(lookup_names = True):
-        print '===================='
+        print '========== discovered device =========='
         pprint(device)
+        print '== device services:'
         service = find_service(device)
         pprint(service)
+        print '=======================================' 
 except IOError:
     print 'No devices found.'
